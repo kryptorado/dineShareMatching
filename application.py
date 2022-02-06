d89656ec8214b42ae9fc24f295985fcad070ba92
Dora Goczi <doribea@gmail.com>
2022-02-06 16:55:39 -0500

    removed unused imports and moved constants outside of fn

--- application.py
+++ application.py
@@ -5,28 +5,25 @@ from utils.remap_ids import remap_ids
 from matching.compatibility import calculate_compatibility
 from matching.matching import solve_ip
 import json
-import sys
+
+RANK_RANGE = 5
+THRESHOLD_COMPATIBILITY = 0.2
+
 
 # TODO: call with actual user data
 def do_matching():
-    # Constants
-    rank_range = 5
-    threshold_compatibility = 0.2
-
     with open("input_test_data/input_test_data_1.json") as json_file:
         json_dict = json.load(json_file)
 
-    user_preferences, num_interests, num_users = read_json(json_dict, rank_range)
+    user_preferences, num_interests, num_users = read_json(json_dict, RANK_RANGE)
     user_preferences, mapping = remap_ids(user_preferences, {}, True)
     user_compatibility_scores, num_users = calculate_compatibility(user_preferences, num_interests, num_users)
-    score_max = pow(rank_range, 2) * num_interests
-    matches = solve_ip(num_users, user_compatibility_scores, threshold_compatibility, score_max)
+    score_max = pow(RANK_RANGE, 2) * num_interests
+    matches = solve_ip(num_users, user_compatibility_scores, THRESHOLD_COMPATIBILITY, score_max)
 
     final_results = remap_ids(matches, mapping, False)
-    print(final_results)
 
-
-    return '<p>Hello %s!</p>\n' % final_results
+    return '<p>%s</p>' % final_results
 
 
 # EB looks for an 'application' callable by default.
